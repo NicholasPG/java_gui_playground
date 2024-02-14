81338e38e22910756bf754dc39e53d64cc59e0dc
NicholasPG <143528104+NicholasPG@users.noreply.github.com>
2024-02-14 16:50:39 -0600

    Added TeamIcon and ShapeIcon

--- TeamPanel.java
+++ TeamPanel.java
@@ -14,7 +14,11 @@ public class TeamPanel extends JPanel {
         memberLabels[0] = new JLabel("Sample", new ImageIcon("smile.png"), SwingConstants.CENTER);
         memberLabels[0].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[0].setVerticalTextPosition(SwingConstants.BOTTOM);
-        memberLabels[1] = new JLabel();
+
+        memberLabels[1] = new JLabel("Nicholas", new ImageIcon("Dancing Khaled.gif"), SwingConstants.CENTER);
+        memberLabels[1].setHorizontalTextPosition(SwingConstants.CENTER);
+        memberLabels[1].setVerticalTextPosition(SwingConstants.TOP);
+
         memberLabels[2] = new JLabel("Asparagus", new ImageIcon("asparagus.gif"), SwingConstants.CENTER);
         memberLabels[2].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[2].setVerticalTextPosition(SwingConstants.TOP);
@@ -25,7 +29,7 @@ public class TeamPanel extends JPanel {
             this.add(member);
         }
 
-        this.setPreferredSize(new Dimension(500, 250));
+        this.setPreferredSize(new Dimension(700, 400));
         this.setBackground(Color.LIGHT_GRAY);
 
 
